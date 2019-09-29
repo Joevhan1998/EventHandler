3bc216e0aa9669483ffe928d34b2b3352134672c
Mich <michelle.tee93@gmail.com>
2019-09-29 10:08:18 +0800

    Admin - Restart supervisor and nginx service

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -13,5 +13,5 @@ sudo chown -R www-data:www-data /var/www/
 
 # Restart services
 sudo -Hu www-data chmod a+x /var/www/backend/server_configs/scripts/gunicorn_django.sh
-#sudo service supervisor restart
-#sudo service nginx restart
+sudo service supervisor restart
+sudo service nginx restart
