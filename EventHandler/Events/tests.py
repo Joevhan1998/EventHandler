03d5e40b35611a613e4420144ced01caae12883d
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-09-29 11:28:16 +0800

    Category Done

--- EventHandler/Events/tests.py
+++ EventHandler/Events/tests.py
@@ -25,14 +25,31 @@ class CategoryViewTest(APITestCase):
         self.assertEqual(expected, actual)
 
     def test_GET_undefined(self):
-        response = self.client.get("/database/categories", {"categoryId": 123})
+        response = self.client.get("/database/categories", data = {"categoryId": 123})
         expected = [value for value in Category.objects.none().values()]
         actual = json.loads(response.render().content)
         self.assertEqual(expected, actual)
 
-    def test_GET_1(self):
-        response = self.client.get("/database/categories", {"categoryId": 1})
-        expected = [value for value in Category.objects.filter(pk = 1).values()]
+    def test_GET_2(self):
+        response = self.client.get("/database/categories", data = {"categoryId": 2})
+        expected = [value for value in Category.objects.filter(pk = 2).values()]
         actual = json.loads(response.render().content)
         self.assertEqual(expected, actual)
+
+    def test_POST(self):
+        response = self.client.post("/database/categories", headers = {"Authorization": "Bearer sometoken"}, data = {"name": "four"})
+        assert None != Category.objects.filter(name = "four")
+
+    def test_PUT_3(self):
+        response = self.client.put("/database/categories", headers = {"Authorization": "Bearer sometoken"}, data = {"categoryId": 3, "name": "four"})
+        expected = {"categoryId": 3, "name": "four"}
+        actual = Category.objects.filter(name = "four").first()
+        actual = {"categoryId": actual.pk, "name": actual.name}
+        self.assertEqual(expected, actual)
+
+    def test_DELETE_4(self):
+        response = self.client.delete("/database/categories", headers = {"Authorization": "Bearer sometoken"}, data = {"categoryId": 4})
+        for pair in Category.objects.all():
+            assert (pair.pk != 4)
+
 
