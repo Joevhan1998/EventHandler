93b6bcde4ec739db2efe737f606606782b56e8ae
FuhankB <buntarafuhank@gmail.com>
2019-09-29 12:12:04 +0800

    date/time

--- EventHandler/Events/urls.py
+++ EventHandler/Events/urls.py
@@ -5,7 +5,9 @@ from . import views
 router = routers.DefaultRouter()
 
 router.register(r'categories',views.CategoryView, base_name='category')
+router.register(r'events\/(?P<from_time>.+)\/(?P<to_time>.+)',views.EventsViewTime, base_name='event-search')
 router.register(r'events',views.EventsView, base_name='event')
 router.register(r'registers',views.RegisterView, base_name='register')
+
 
 urlpatterns = router.urls
