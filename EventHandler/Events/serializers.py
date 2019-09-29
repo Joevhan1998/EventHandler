0b18e43f936afc839a7981c666fcdbcef31880ef
FuhankB <buntarafuhank@gmail.com>
2019-09-29 08:58:32 +0800

    enable swagger for queryparameter

--- EventHandler/Events/serializers.py
+++ EventHandler/Events/serializers.py
@@ -1,19 +1,19 @@
 from rest_framework import serializers
 from .models import *
 
-
-class EventSerializers(serializers.HyperlinkedModelSerializer) :
-    class Meta :
-        model       = Event
-        fields      = '__all__'
-
-class RegisterSerializers(serializers.HyperlinkedModelSerializer) :
+class RegisterSerializers(serializers.ModelSerializer) :
     class Meta :
         model       = Register
         fields      = '__all__'
 
 
-class CategorySerializers(serializers.HyperlinkedModelSerializer) :
+class CategorySerializers(serializers.ModelSerializer) :
     class Meta :
         model       = Category
         fields      = ('name', )
+
+class EventSerializers(serializers.ModelSerializer) :
+    category = CategorySerializers(many=True)
+    class Meta :
+        model       = Event
+        fields      = '__all__'
--- EventHandler/Events/views.py
+++ EventHandler/Events/views.py
@@ -3,11 +3,34 @@ from rest_framework import filters
 from .serializers import *
 from .models import *
 
+from rest_framework.filters import BaseFilterBackend
+import coreapi
+
+class SimpleFilterBackend(BaseFilterBackend):
+    def get_schema_fields(self, view):
+        return [coreapi.Field(
+            name='eventId',
+            location='query',
+            required=False,
+            type='string'
+        )]
+
 class EventsView(viewsets.ModelViewSet) :
-	queryset = Event.objects.all()
+	# queryset = Event.objects.all()
+	filter_backends = (SimpleFilterBackend,)
 	serializer_class = EventSerializers
-	filter_backends = [filters.SearchFilter]
-	search_fields = ['name']
+	# filter_backends = [filters.SearchFilter]
+	# search_fields = ['name']
+	
+	def filter_queryset(self, queryset):
+		return queryset
+
+	def get_queryset(self):
+		queryset = Event.objects.all()
+		eventId = self.request.query_params.get('eventId', None)
+		if eventId is not None:
+			queryset = queryset.filter(pk=eventId)
+		return queryset
 
 class CategoryView(viewsets.ModelViewSet) :
     queryset            = Category.objects.all()
