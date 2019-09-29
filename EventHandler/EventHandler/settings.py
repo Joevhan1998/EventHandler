5cd2df5890c69dbcd9583d552a9fc54cfcb18826
Nydia <nyd.bubble@gmail.com>
2019-09-29 14:11:09 +0800

    Added Django Cors middleware

--- EventHandler/EventHandler/settings.py
+++ EventHandler/EventHandler/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'rest_framework',
+    'corsheaders',
     'Events'
 ]
 
@@ -55,9 +56,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'EventHandler.urls'
 
