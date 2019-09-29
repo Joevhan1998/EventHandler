6e4624388730eda320fded23e7576572c8b1709a
FuhankB <buntarafuhank@gmail.com>
2019-09-29 10:26:36 +0800

    adding the category id

--- EventHandler/Events/urls.py
+++ EventHandler/Events/urls.py
@@ -6,6 +6,7 @@ router = routers.DefaultRouter()
 
 router.register(r'categories',views.CategoryView, base_name='category')
 router.register(r'events',views.EventsView, base_name='event')
+router.register(r'events/APIsearch',views.EventsViewSearch, base_name='event-search')
 router.register(r'registers',views.RegisterView, base_name='register')
 
 urlpatterns = router.urls
--- EventHandler/Events/views.py
+++ EventHandler/Events/views.py
@@ -6,21 +6,31 @@ from .models import *
 from rest_framework.filters import BaseFilterBackend
 import coreapi
 
-class SimpleFilterBackend(BaseFilterBackend):
+class EventFilterBackend(BaseFilterBackend):
     def get_schema_fields(self, view):
         return [coreapi.Field(
             name='eventId',
             location='query',
             required=False,
             type='string'
+        ), coreapi.Field(
+        	name='categoryId',
+        	Location='query',
+        	required=False,
+        	type='string' 
         )]
+
+class EventsViewSearch(viewsets.ModelViewSet) :
+	queryset = Event.objects.all()
+	serializer_class = EventSerializers
+	filter_backends = (filters.SearchFilter,)
+	search_fields = ['name']
 
 class EventsView(viewsets.ModelViewSet) :
 	# queryset = Event.objects.all()
-	filter_backends = (SimpleFilterBackend,)
+	filter_backends = (EventFilterBackend,)
 	serializer_class = EventSerializers
 	# filter_backends = [filters.SearchFilter]
-	# search_fields = ['name']
 	
 	def filter_queryset(self, queryset):
 		return queryset
@@ -28,8 +38,11 @@ class EventsView(viewsets.ModelViewSet) :
 	def get_queryset(self):
 		queryset = Event.objects.all()
 		eventId = self.request.query_params.get('eventId', None)
+		categoryId = self.request.query_params.get('categoryId', None)
 		if eventId is not None:
 			queryset = queryset.filter(pk=eventId)
+		if categoryId is not None:
+			queryset = queryset.filter(category__id=categoryId)
 		return queryset
 
 class CategoryView(viewsets.ModelViewSet) :
